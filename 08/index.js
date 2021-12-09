55ffeab8fca83c78219c28ca76b191ae1c901e14
Lubos Muller <lubos.muller@citrix.com>
2021-12-09 12:32:14 +0100

    task 8.2 unfinished

--- 08/index.js
+++ 08/index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const data = fs.readFileSync(path.join(__dirname, "input.txt")).toString().split('\n')
+const data = fs.readFileSync(path.join(__dirname, "input_test.txt")).toString().split('\n')
     .map(row => row.split(' | '));
 
 //data[0] input
@@ -20,13 +20,36 @@ let task1 = data => {
     return counter;
 
 }
-console.log(task1(data));
+//console.log(task1(data));
 
 let task2 = data => {
 
+    let segments = new Array(7).fill('');
+    console.log(segments);
+
+
+    let counter = 0;
+    for (let i = 0; i < data.length; i++) {
+        let signals = [...data[i][1].split(' ')];
+        //console.log(signals.join(' '))
+        // find the pattern
+
+        // 1
+        //console.log(Array.from(new Set(signals.filter(d => d.length === 2))))
+
+        // 4
+        //console.log(Array.from(new Set(signals.filter(d => d.length === 4))))
+
+        // 7
+        console.log(Array.from(new Set(signals.filter(d => d.length === 3))))
+
+        // 8
+        //console.log(Array.from(new Set(signals.filter(d => d.length === 7))))
+
+    }
 
 }
-//console.log(task2(data));
+console.log(task2(data));
 
 module.exports = {
     task1, task2
